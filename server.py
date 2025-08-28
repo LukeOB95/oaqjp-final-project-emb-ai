71f61be818498c2b339ca68a74e902a0d78736ef
LukeOB95 <luke.oboyle@gmail.com>
2025-08-28 11:39:31 -0400

    August 28th Changes

--- server.py
+++ server.py
@@ -3,8 +3,8 @@ from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detection")
 
-@app.route("/emotionDetector", methods=['GET', 'POST'])
-def give_results():
+@app.route("/emotionDetector")
+def emotion_detector_function():
 
     # Text to analyze
     text_to_analyse = request.args.get("textToAnalyze")
@@ -12,32 +12,17 @@ def give_results():
     # Response
     response = emotion_detector(text_to_analyse)
 
-    # Emotions
-    anger = response['anger']
-    disgust = response['disgust']
-    fear = response['fear']
-    joy = response['joy']
-    sadness = response['sadness']
-    dominant_emotion = response['dominant_emotion']
+    if response['dominant_emotion'] is None:
+        response_text = "Invalid input! Please try again."
 
-    # Details - For the return statement
-    details = {
-        "anger": anger,
-        "disgust": disgust,
-        "fear": fear,
-        "joy": joy,
-        "sadness": sadness,
-        'dominant_emotion': dominant_emotion
-    }
+    else:
+        response_text = f"For the given statement, the system response is 'anger': \
+                    {response['anger']}, 'disgust': {response['disgust']}, \
+                    'fear': {response['fear']}, 'joy': {response['joy']}, \
+                    'sadness': {response['sadness']}. The dominant emotion is \
+                    {response['dominant_emotion']}."
 
-    if request.method == 'GET':
-        return f"For the given statement, the system response is 'anger': {details['anger']}, 'disgust': {details['disgust']}, 'fear': {details['fear']}, 'joy': {details['joy']}, 'sadness': {details['sadness']}. The most dominant emotion is {details['dominant_emotion']}."
-
-    elif request.method == 'GET' and text_to_analyse is None:
-        return f"Cannot analyze"
-    
-    if not dominant_emotion:
-        return {"error message": "Blank input"}, 400
+    return response_text
 
 @app.route("/")
 def render_index_page():
